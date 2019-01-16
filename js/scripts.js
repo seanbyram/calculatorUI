6863619dfe24ce368bb7317b5f128bd03f454322
Student <example@example.com>
2019-01-16 11:43:54 -0800

    update form with UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 
 // Business Logic
-alert("test")
 
 var add = function(number1, number2) {
   return number1 + number2;
@@ -22,12 +21,14 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() {
 
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
 
-  alert(add(number1, number2));
-
-
+  });
 
 
 
